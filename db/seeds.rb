35edc878317a052144c601cee43376a5c3dd3eb7
Sam Schlicht <samschlicht@gmail.com>
2023-02-11 19:54:19 +1100

    just saving progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-puts "Destroying all users"
 User.destroy_all
-puts "Destroying all places"
+puts "Destroyed all users"
 Place.destroy_all
-puts "Destroying all notifications"
+puts "Destroyed all places"
 Notification.destroy_all
-puts "Destroying all bookmarks"
+puts "Destroyed all notifications"
 Bookmark.destroy_all
-puts "Destroying all interests"
+puts "Destroyed all bookmarks"
 Interest.destroy_all
+puts "Destroyed all interests"
 
 User.create!(
   name: 'samschlicht',
@@ -69,4 +69,30 @@ User.create!(
   phone_number: '+818067915731',
 ).photo.attach(io: File.open('app/assets/images/ryan.jpg'), filename: 'ryan.jpg', content_type: 'image/jpg')
 
-puts "Created unique users, one for each member of our team"
+puts "Created 4 unique users, one for each member of our team"
+
+interests = %w(coffee cocktails ramen sushi izakayas records guitars books gifts art museums parks clothes plants design)
+
+interests.length.times do |i|
+  Interest.create!(
+    name: interests[i-1]
+  )
+end
+
+puts "Created #{interests.length} interests"
+
+# getting places from the Google Places API
+# below is code pasted from their documentation:
+
+require "uri"
+require "net/http"
+
+url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522%2C151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyAxwvCN31ixCApVWUY7L_b1N-qw2d0YpYM")
+
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+
+request = Net::HTTP::Get.new(url)
+
+response = https.request(request)
+puts response.read_body
