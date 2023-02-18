2f2f08c12410c940e8c43d27cef7c8ce11a7a9f3
Isabel Figueroa Estrada <isabelfigueroaestrada@gmail.com>
2023-02-18 15:58:31 +0900

    temporary google api

--- app/services/interest_search.rb
+++ app/services/interest_search.rb
@@ -5,9 +5,9 @@ require 'yaml'
 class InterestSearch
   def self.search
     # this is the url for a 'nearby search' using the Google Places API, specifying the Hub in Meguro as the location and 10km as the radius
-    interests = %w(cafe cocktail-bar nightclub ramen sushi izakaya record-store guitar-store book-store gift-store gallery museum park clothes-store plant-store design-store jewelry-store temple monument)
+    interests = %w(cafe cocktail-bar nightclub ramen)
     puts interests
-    url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=35.6345266519309,139.70829303068464&radius=5000&keyword=#{@keyword}&key=#{ENV.fetch(‘GOOGLE_MAPS_API_KEY’)}")
+    url = URI("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=35.6345266519309,139.70829303068464&radius=5000&keyword=#{@keyword}&key=AIzaSyCqJKoNQrQGewoxaSaj6fTSinJLntgKNrI")
     interests.each do |interest|
       filepath = "./app/services/#{interest}.json"
       File.new(filepath, "w")
